c6badd2600c5d57170868041de6507a535d4513e
madhuriprao <madhurirao014@gmail.com>
2024-10-08 15:46:08 +0200

    Added a quiz question, added promise handling with .then(), .catch()

--- promise.js
+++ promise.js
@@ -1,19 +1,35 @@
-const myPromise = new Promise((resolve, reject)=> {
-let success = true;
+const myQuizPromise = new Promise((resolve, reject)=> {
+
+    // Quiz question and options
+    const question = "What do cats do for about 16 hours a day? ";
+    const options = [
+        "a) Run a marathon",
+        "b) Plot world domination",
+        "c) Nap like there's no tomorrow",
+        "d) Pratice their meowing skills"
+    ];
+
+    // Display the question and options
+    console.log(question);
+    options.forEach(option => console.log(option));
+
+    const answer = "c) Nap like there's no tomorrow";  // Change this to test different answers. 
     
-        if (success) {
-            resolve("Promise is resolved");
-        
-        } else {
-            reject("Operation was rejected");
-        }
-    });
+    if (answer === "c) Nap like there's no tomorrow") {
+        resolve("Congratulations! You resolved the quiz correctly.");
+    } else {
+        reject("Sorry, that's not the right answer. Try again!");
+    }
+
+});
+
+
     
-    myPromise
+myQuizPromise
         .then((message)=>{
         console.log(message);
     })
         .catch((error)=>{
-        console.log("error:" + error
+        console.log("Error:" + error
         );
     })
